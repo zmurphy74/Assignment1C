a6272101283806729d5784b19e0f7887c5016ab9
Zach Murphy <zmurphy74@yahoo.com>
2018-01-30 12:29:49 -0600

    Add project files.

---
+++ Assignment1C/ATM.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Runtime.InteropServices;
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+namespace  project1
+{
+  
+    public class ATM
+    {
+        static ATM atm = new ATM();
+        bool alreadyExecuted;
+        Account[] accounts = new Account[3];
+
+		public static void Main(string[] args)
+		{
+            atm.menu();
+		}
+
+        public void menu()
+		{
+			int input;
+			do
+			{
+				Console.WriteLine("1. Populate Accounts");
+				Console.WriteLine("2. Select Accounts");
+                Console.WriteLine("3. Save Accounts");
+				Console.WriteLine("4. Load Accounts");
+                Console.WriteLine("5. Exit");
+				input = Convert.ToInt32(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    if (!alreadyExecuted)
+                    {
+                        atm.populate();
+                        alreadyExecuted = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nAccounts already populated!");
+                    }
+                }
+                else if (input == 2)
+                {
+                    if (alreadyExecuted)
+                    {
+                        atm.select();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Populate accounts first.");
+                    }
+                }
+                else if (input == 3)
+                {
+                    
+                    try
+                    {
+                        if (alreadyExecuted)
+                        {
+                            XmlSerializer serializer = new XmlSerializer(accounts.GetType());
+                            //FileStream stream = new FileStream("bank.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                            StreamWriter writer = new StreamWriter("bank.xml");
+                            serializer.Serialize(writer, accounts);
+                            writer.Close();
+                            alreadyExecuted = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("There are no accounts to save!");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.InnerException);
+                    }
+                }
+                else if (input == 4)
+                {
+                    try
+                    {
+                        XmlSerializer deserializer = new XmlSerializer(accounts.GetType());
+                        //FileStream rereader = new FileStream("bank.xml", FileMode.Open, FileAccess.ReadWrite);
+                        StreamReader sr = new StreamReader("bank.xml");
+                        accounts = (Account[])deserializer.Deserialize(sr);
+                        sr.Close();
+                        alreadyExecuted = true;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }                     
+                }
+                else if (input == 5)
+                {
+                Environment.Exit(0);
+                }
+				else if (input > 5 || input < 1)
+				{
+				Console.WriteLine("\nPlease enter a valid number.\n");
+				}
+			} while (input != 5);
+		}
+
+        public void populate()
+        {
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                accounts[i] = new Account();
+            }
+        }
+
+        public void select()
+        {
+            int input;
+            do
+            {
+                Console.WriteLine("Enter account number: ");
+                input = Convert.ToInt32(Console.ReadLine());
+
+                if (input >= 4 || input <= 0)
+                {
+                    Console.WriteLine("Please enter numbers 1 2 or 3");
+                }
+
+            }
+            while (!((input - 1) >= 0) || !((input - 1) < accounts.Length));
+                {
+                try
+                {
+                    accounts[input - 1].menu();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+		}
+    }
+
+}
---
+++ Assignment1C/Account.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace project1
+{
+
+[Serializable()]
+
+	public class Account
+	{
+        public double balance;
+		double amount;
+		public int firstdate;
+		public int seconddate;
+		public DateTime cal1 = new DateTime();
+		public DateTime cal2 = new DateTime();
+		public bool dateflag = false;
+		double rate;
+
+
+        public Account()
+        {
+            balance = 100;
+        }
+
+
+        public void menu()
+		{
+			int input;
+			do
+			{
+			Console.WriteLine("Enter 1 to deposit.");
+			Console.WriteLine("Enter 2 to Withdraw.");
+			Console.WriteLine("Enter 3 to Check Balance.");
+			Console.WriteLine("Enter 4 to exit.");
+			input = 0;
+			input = Convert.ToInt32(Console.ReadLine());
+				switch (input)
+				{
+					case 1:
+				        if (dateflag == true)
+				        {
+						    getDate2();
+						    getInterest();
+						    deposit();
+				        }
+				        else
+				        {
+						    getDate1();
+						    deposit();
+				        }
+						break;
+					case 2:
+				        if (dateflag == true)
+				        {
+						    getDate2();
+						    getInterest();
+						    withdraw();
+				        }
+				        else
+				        {
+						    getDate1();
+						    withdraw();
+				        }
+						break;
+					case 3:
+						if (dateflag == true)
+						{
+						    getDate2();
+						    getInterest();
+						    display();
+						}
+				        else
+				        {
+						getDate1();
+						display();
+				        }
+						break;
+					default:
+						break;
+				}
+			}
+            while (!(input >= 4 || input <= 0));
+		}
+		public void getDate1()
+		{
+            Console.Write("Enter todays date(mm/dd/yyyy): ");
+		    string inputText = Convert.ToString(Console.ReadLine());
+            string pattern = "MM/dd/yy";
+            DateTime parsedDate;
+            DateTime.TryParseExact(inputText, pattern, null,
+            DateTimeStyles.None, out parsedDate);
+            DateTime date = DateTime.Parse(inputText);
+		    cal1 = date;
+            firstdate = cal1.DayOfYear;
+            dateflag = true;
+		}
+	    public void getDate2()
+	    {
+	        Console.Write("Enter todays date(mm/dd/yyyy): ");
+			string inputText = Convert.ToString(Console.ReadLine());
+			DateTime date = DateTime.Parse(inputText);
+            date.ToString("d");
+			cal2 = date;
+			seconddate = cal2.DayOfYear;
+
+				if (firstdate > seconddate)
+					{
+						Console.WriteLine("You must enter a future date.");
+						getDate2();
+					}
+	    }
+
+		public void getInterest()
+		{
+			int datediff = seconddate - firstdate;
+			rate = .10 / 365;
+			double ratetime = Math.Pow(1 + rate,datediff);
+			balance = balance * ratetime;
+			firstdate = seconddate;
+		}
+		public int deposit()
+		{
+			Console.Write("Enter amount to deposit:");
+		    amount = Convert.ToDouble(Console.ReadLine());
+				if (amount < 0)
+				{
+				    Console.WriteLine("Invalid Amount");
+				    return 1;
+				}
+				balance = balance + amount;
+				return 0;
+		}
+
+		public int withdraw()
+		{
+            string balance1 = String.Format("Balance: {0:C}", balance);
+            Console.WriteLine(balance1);
+			Console.Write("Enter amount to withdraw:");
+		    amount = Convert.ToDouble(Console.ReadLine());
+				if (balance < amount)
+				{
+				Console.WriteLine("Not sufficient balance.");
+				return 1;
+				}
+				    if (amount < 0)
+				    {
+				    Console.WriteLine("Invalid Amount");
+				    return 1;
+				    }
+				    balance = balance - amount;
+				    return 0;
+		}
+		public void display()
+		{
+            string balance1 = String.Format("Balance: {0:C}", balance);
+		    Console.WriteLine(balance1);
+
+		}
+
+	}
+
+}
